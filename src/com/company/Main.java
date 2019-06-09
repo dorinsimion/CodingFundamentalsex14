ffea42f3352814cfcb7a9de90b421581f01832f4
Dorin Simion <dorin_Simion_1987@yahoo.com>
2019-06-09 15:47:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int random = new Random().nextInt(1000)+1;
+        int attempts = 0;
+        int guess;
+        Scanner input = new Scanner(System.in);
+        do{
+            guess=input.nextInt();
+            if(guess<random)
+                System.out.println("Introduceti un numar mai mare!");
+            else if(guess>random)
+                System.out.println("Introduceti un numar mai mic");
+            attempts++;
+        }while(guess!=random);
+
+        System.out.println("Numarul este "+random
+                +". L-ati nimerit din "+ attempts + " incercari.");
+    }
+
+}
